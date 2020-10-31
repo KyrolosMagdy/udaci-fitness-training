1e5abc961a8e64c6589dc71eb02966a194f6c5bb
mina <>
2020-10-31 14:37:34 +0200

    debugging 2

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
 import * as React from "react";
-import { View } from "react-native";
+import { View, Platform, StatusBar } from "react-native";
 import AddEntry from "./components/AddEntry";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
+// import { TabNavigator } from "react-navigation";
 // You can import from local files
-import History from "./components/History"
+import History from "./components/History";
+import { purple, white, gray, orange } from "./utils/colors";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import Constants from "expo-constants";
+
+// import {
+//   createDrawerNavigator,
+//   createStackNavigator,
+//   createBottomTabNavigator,
+//   createAppContainer,
+// } from "react-navigation";
+
+const Tab = createMaterialTopTabNavigator();
+function TabNaviagtor() {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: white,
+        inactiveTintColor: gray,
+        style: { backgroundColor: purple },
+        tabStyle: { height: 54 },
+        indicatorStyle: { backgroundColor: orange },
+      }}
+    >
+      <Tab.Screen name="History" component={History} />
+      <Tab.Screen name="Add Entry" component={AddEntry} />
+    </Tab.Navigator>
+  );
+}
+
+const UdaciStatusBar = ({ backgroundColor, ...props }) => {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+};
 
 export default function App() {
   const store = createStore(reducer);
   return (
     <Provider store={store}>
       <View style={{ flex: 1 }}>
-        <View style={{ height: 20}} />
-        <History />
-        
-        
+        <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+        <NavigationContainer>
+          <TabNaviagtor />
+        </NavigationContainer>
       </View>
     </Provider>
   );
--- components/History.js
+++ components/History.js
@@ -1,52 +1,115 @@
-import React, { useEffect, useState } from "react";
-import { View, Text } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
 import { connect } from "react-redux";
-import {addEntry, receiveEntries} from "../actions";
-import {timeToString, getDailyReminderValue} from "../utils/helpers"
-import { fetchCalenderResults } from "../utils/api"
-import entries from "../reducers";
-import UdacityFitnessCalendar from 'udacifitness-calendar-fix'
+import { addEntry, receiveEntries } from "../actions";
+import { timeToString, getDailyReminderValue } from "../utils/helpers";
+import { fetchCalenderResults } from "../utils/api";
+import UdacityFitnessCalendar from "udacifitness-calendar-fix";
+import { white } from "../utils/colors";
+import DateHeader from "./DateHeader";
+import MetricCard from "./MetricCard";
+import { AppLoading } from "expo";
 
 class History extends React.Component {
+  state = {
+    ready: false,
+  };
 
   componentDidMount() {
-    const {dispatch} = this.props;
-     fetchCalenderResults().then((entries) => dispatch(receiveEntries(entries))).then(entries => {
-      if (!entries[timeToString()]) {
-        dispatch(addEntry({
-          [timeToString()]: getDailyReminderValue()
-        }))
-      }
-    })
+    const { dispatch } = this.props;
+    fetchCalenderResults()
+      .then((entries) => dispatch(receiveEntries(entries)))
+      .then((entries) => {
+        if (!entries[timeToString()]) {
+          dispatch(
+            addEntry({
+              [timeToString()]: getDailyReminderValue(),
+            })
+          );
+        }
+      })
+      .then(() => {
+        this.setState({
+          ready: true,
+        });
+      });
   }
 
-  renderItem = ({ today, ...metrics}, formatedDate, key) => (
-    <View>
-      {
-        today? <Text> { JSON.stringify(today)}</Text> : <Text> { JSON.stringify(metrics)} </Text>
-      }
+  renderItem = ({ today, ...metrics }, formatedDate, key) => (
+    <View style={styles.item}>
+      {today ? (
+        <View>
+          <DateHeader date={formatedDate} />
+          <Text style={styles.noDataText}> {today} </Text>
+        </View>
+      ) : (
+        <TouchableOpacity onPress={() => console.log("pressed!")}>
+          <MetricCard metrics={metrics} date={formatedDate} />
+        </TouchableOpacity>
+      )}
     </View>
-  )
+  );
 
   renderEmptyDate = (formatedDate) => (
-    <View>
-      <Text> No Data for this day</Text>
+    <View style={styles.item}>
+      <DateHeader date={formatedDate} />
+      <Text style={styles.noDataText}>
+        {" "}
+        You didn't logged any data on this day.{" "}
+      </Text>
     </View>
-  )
-  
+  );
+
   render() {
-    const { entries} = this.props;
-      return(
-  
-      <UdacityFitnessCalendar items={entries} renderItem={this.renderItem} renderEmptyDate={this.renderEmptyDate} />
-  
-  )
+    const { entries } = this.props;
+    const { ready } = this.state;
+    if (!ready) {
+      return <AppLoading />;
+    }
+    return (
+      <UdacityFitnessCalendar
+        items={entries}
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+      />
+    );
   }
 }
 
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === "ios" ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: "center",
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: "rgba(0, 0, 0, 0.24)",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+  },
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+});
 
-const mapStateToProps = entries => {
-  return entries;
-} 
+function mapStateToProps(entries) {
+  return {
+    entries,
+  };
+}
 
-export default connect(mapStateToProps)(History)
+export default connect(mapStateToProps, null)(History);
